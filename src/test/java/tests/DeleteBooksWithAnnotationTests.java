96104671f32a9c4a1c3a7f00e614cc7931f842dd
dar-ia <iriryt@gmail.com>
2024-04-25 17:26:07 +0400

    Correctness

--- src/test/java/tests/DeleteBooksWithAnnotationTests.java
+++ src/test/java/tests/DeleteBooksWithAnnotationTests.java
@@ -1,20 +1,16 @@
 package tests;
 
-import api.AccountApis;
 import api.BookStoreApis;
 import helpers.WithLogin;
-import models.accountmodels.UserModel;
 import org.junit.jupiter.api.*;
 import pages.CartPage;
 
 import static api.AuthApi.authorizeRequest;
-import static io.qameta.allure.Allure.step;
 
 @DisplayName("Tests on books removing from the cart")
 public class DeleteBooksWithAnnotationTests extends TestBase {
     CartPage cartPage = new CartPage();
     BookStoreApis bookStoreApis = new BookStoreApis();
-    AccountApis accountApis = new AccountApis();
 
     @Test
     @WithLogin
@@ -22,18 +18,10 @@ public class DeleteBooksWithAnnotationTests extends TestBase {
     void deleteAllBooksFromTheCartTest() {
         bookStoreApis.deleteAllBooks(authorizeRequest().getUserId());
         bookStoreApis.addBooks();
-        UserModel userWithBooks = accountApis.getUserInfo();
-        step("Assert that cart is not empty in API", () -> {
-            Assertions.assertEquals(2, userWithBooks.getBooks().length);
-        });
         cartPage.openBrowserOnTheCart()
                 .assertSuccessLogin(authorizeRequest().getUsername())
                 .assertThatCartIsNotEmpty();
         bookStoreApis.deleteAllBooks(authorizeRequest().getUserId());
-        UserModel userIsCleared = accountApis.getUserInfo();
-        step("Assert that cart is not empty in API", () -> {
-            Assertions.assertEquals(0, userIsCleared.getBooks().length);
-        });
         cartPage.refreshPage()
                 .assertThatCartIsEmpty();
     }
